edc930e229fc7b976069e92732648b362d0d9668
Luong Viet Dung <luong.viet.dung@framgia.com>
2015-07-17 22:08:31 +0700

    Delete All

--- app/models/comment.rb
+++
@@ -1,6 +0,0 @@
-class Comment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :entry
-  default_scope -> { order(created_at: :desc) }
-  validates :content, presence: true
-end
--- app/models/entry.rb
+++
@@ -1,9 +0,0 @@
-class Entry < ActiveRecord::Base
-  belongs_to :user
-  default_scope -> { order(created_at: :desc) }
-  
-  has_many :comments
-  
-  validates :title, presence: true, length: {minimum: 10}
-  validates :body, presence: true, length: {minimum: 255}
-end
